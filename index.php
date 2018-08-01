89472d5df743968bad6d2b3f64ce4c27676eb57a
Martin <martin.prazak@gmail.com>
2018-08-01 22:21:02 +0100

    Added detailed graph

--- index.php
+++ index.php
@@ -20,12 +20,17 @@
 <body>
 
 <div style="width:75%;">
-    <canvas id="canvas"></canvas>
+    <canvas id="canvas_overall"></canvas>
+</div>
+
+<div style="width:75%;">
+    <canvas id="canvas_detailed"></canvas>
 </div>
 
 <?php
 
 $file = fopen('massif.out.5055', 'r');
+$stacktrace_keys = array();
 $snapshots = array();
 
 while (($line = fgets($file)) !== false) {
@@ -43,6 +48,25 @@ while (($line = fgets($file)) !== false) {
 
     else if(preg_match('/^mem_stacks_B=([0-9]+)$/', $line, $matches))
         $snapshots[count($snapshots)-1]['mem_stacks_B'] = $matches[1];
+
+    else if(preg_match('/^heap_tree=([a-z]+)$/', $line, $matches))
+        $snapshots[count($snapshots)-1]['heap_tree'] = $matches[1];
+
+    // else if(preg_match('/^[ ]*n([0-9]+): ([0-9]+) (.*)$/', $line, $matches))
+    //     print_r($matches);
+
+    // only first-level stacktraces for the graph
+    else if(preg_match('/^ n([0-9]+): ([0-9]+) (.*)$/', $line, $matches)) {
+        if(strpos($matches[3], 'all below massif') === false) {
+            $stacktrace_keys[$matches[3]] = true;
+            $snapshots[count($snapshots)-1]['stacktrace'][$matches[3]] = $matches[2];
+        }
+        else {
+            $stacktrace_keys['below threshold'] = true;
+            $snapshots[count($snapshots)-1]['stacktrace']['below threshold'] = $matches[2];
+        }
+    }
+
 }
 
 fclose($file);
@@ -50,6 +74,113 @@ fclose($file);
 /////////////////////////////////
 
 //echo('<script>');
+
+    function colour($val) {
+        $elems = array(
+            array(1,0,0),
+            array(1,1,0),
+            array(0,1,0),
+            array(0,1,1),
+            array(0,0,1),
+            array(0,0,0),
+        );
+
+        $v = $val * floatval((count($elems)-1));
+
+        $weight = $v - floor($v);
+        // echo $weight;
+
+        // echo $elems[intval(floor($v))][0] * (1.0 - $weight);
+
+        $elem = array();
+        $elem[0] = $elems[intval(floor($v))][0] * (1 - $weight) + $elems[intval(ceil($v))][0] * ($weight);
+        $elem[1] = $elems[intval(floor($v))][1] * (1 - $weight) + $elems[intval(ceil($v))][1] * ($weight);
+        $elem[2] = $elems[intval(floor($v))][2] * (1 - $weight) + $elems[intval(ceil($v))][2] * ($weight);
+
+        return 'rgb('.($elem[0]*255).",".($elem[1]*255).",".($elem[2]*255).")";
+        // return 'rgb(0,0,0)';
+    }
+
+?>
+
+    <script>
+        var config_detailed = {
+            type: 'line',
+            data: {
+                labels: [
+<?php
+                foreach($snapshots as $value)
+                    if(array_key_exists("stacktrace", $value))
+                        echo($value["time"].",");
+?>
+                ],
+                datasets: [
+<?php
+                $index = 0;
+                foreach($stacktrace_keys as $key => $value) {
+                    $color = $index / (count($stacktrace_keys)-1);
+                    $index++;
+
+                    echo("{\n");
+                    echo("label: '".addslashes($key)."',\n");
+                    echo("borderColor: '".colour($color)."',\n");
+                    echo("borderWidth: 1,\n");
+                    echo("backgroundColor: '".colour($color)."',\n");
+                    echo("data: [");
+
+                    foreach($snapshots as $snapshot)
+                        if(array_key_exists('stacktrace', $snapshot)) {
+                            if(array_key_exists($key, $snapshot['stacktrace']))
+                                echo($snapshot['stacktrace'][$key].',');
+                            else
+                                echo("0,");
+                        }
+
+                    echo("]");
+                    echo("},");
+                }
+?>
+                ]
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Massif detailed output graph'
+                },
+                tooltips: {
+                    mode: 'index',
+                },
+                hover: {
+                    mode: 'index'
+                },
+                legend: {
+                   display: false
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Time sample'
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Size (bytes)'
+                        }
+                    }]
+                }
+            }
+        };
+
+    </script>
+
+<!-- --------------------------------------------------------------------------- -->
+
+<?php
+
 ?>
 
     <script>
@@ -63,7 +194,7 @@ fclose($file);
             grey: 'rgb(201, 203, 207)'
         };
 
-        var config = {
+        var config_overall = {
             type: 'line',
             data: {
                 labels: [
@@ -75,8 +206,8 @@ fclose($file);
                 datasets: [
                 {
                     label: 'mem_heap_B',
-                    borderColor: window.chartColors.red,
-                    backgroundColor: window.chartColors.red,
+                    borderColor: window.chartColors.blue,
+                    backgroundColor: window.chartColors.blue,
                     data: [
 <?php
                     foreach($snapshots as $value)
@@ -138,9 +269,19 @@ fclose($file);
         };
 
         window.onload = function() {
-            var ctx = document.getElementById('canvas').getContext('2d');
-            window.myLine = new Chart(ctx, config);
+            var ctx = document.getElementById('canvas_overall').getContext('2d');
+            window.myLine = new Chart(ctx, config_overall);
+
+            var ctx = document.getElementById('canvas_detailed').getContext('2d');
+            window.myLineDetailed = new Chart(ctx, config_detailed);
+
+            document.getElementById("canvas_detailed").onclick = function(evt){
+                var activePoints = window.myLineDetailed.getElementsAtEvent(evt);
+                // use _datasetIndex and _index from each element of the activePoints array
+                alert(activePoints);
+            };
         };
+
 
     </script>
 
